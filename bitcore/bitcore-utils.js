dc9ffab56b58ba23caf6a4055fb3d019c22edd30
Federico Franzoni <frz.dev@gmx.com>
2019-05-17 15:29:35 +0200

    Reorganizing

---
+++ bitcore/bitcore-utils.js
@@ -0,0 +1,8 @@
+/* btcutils wrapper */
+
+const btcutils = require('./bitcore-addr-utils')
+const btcutils = require('./bitcore-wallet-utils')
+const Mainnet = bitcore.Networks.mainnet
+const Testnet = bitcore.Networks.testnet
+
+
